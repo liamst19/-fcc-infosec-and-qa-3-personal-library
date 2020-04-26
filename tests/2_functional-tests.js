83d3eff4a3ad1dc30764b62eda5ce45e634f231f
Glitch (glitter-vast-caraway) <none>
2020-04-26 15:53:42 +0000

    🐌🐠 Checkpoint ./tests/2_functional-tests.js:15054522/1075

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -23,7 +23,6 @@ suite('Functional Tests', function() {
      chai.request(server)
       .get('/api/books')
       .end(function(err, res){
-       console.log(res.body);
         assert.equal(res.status, 200);
         assert.isArray(res.body, 'response should be an array');
         assert.property(res.body[0], 'commentcount', 'Books in array should contain commentcount');
@@ -55,7 +54,14 @@ suite('Functional Tests', function() {
       });
       
       test('Test POST /api/books with no title given', function(done) {
-        //done();
+       chai.request(server)
+        .post('/api/books')
+        .send({
+         })
+        .end((err, res) => {
+           assert.equal(res.status, 400);
+           done();
+         })
       });
       
     });
@@ -64,9 +70,17 @@ suite('Functional Tests', function() {
     suite('GET /api/books => array of books', function(){
       
       test('Test GET /api/books',  function(done){
-        //done();
-      });      
-      
+       chai.request(server)
+        .get('/api/books')
+        .end(function(err, res){
+          assert.equal(res.status, 200);
+          assert.isArray(res.body, 'response should be an array');
+          assert.property(res.body[0], 'commentcount', 'Books in array should contain commentcount');
+          assert.property(res.body[0], 'title', 'Books in array should contain title');
+          assert.property(res.body[0], '_id', 'Books in array should contain _id');
+          done();
+        });      
+      });  
     });
 
 
